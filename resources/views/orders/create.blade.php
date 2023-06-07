a6ca593bfab9a27f44f6365fe73f1c38bcac60a9
Victor <victor.lvl47@gmail.com>
2023-06-06 23:39:47 -0600

    created a form to add a new order

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -1,3 +1,58 @@
 <x-layout>
-    <h1>TODO: Agregar nuevo pedido form</h1>
+    <h1 class="mt-2 mb-2 text-4xl leading-none tracking-tight text-gray-900 md:text-5xl lg:text-1xl dark:text-white text-center">
+        Agregar Pedido
+    </h1>
+    <div class="flex items-center justify-center min-h-screen bg-gray-900 mt-10 mb-10">
+        <div class="w-full max-w-lg">
+          <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div class="mb-4">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="nombre">
+                Nombre
+              </label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="nombre" type="text" placeholder="Ingrese el nombre">
+            </div>
+            <div class="mb-4">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="tipo_pedido">
+                Tipo de Pedido
+              </label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="tipo_pedido" type="text" placeholder="Ingrese el tipo de pedido">
+            </div>
+            <div class="mb-4">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="descripcion">
+                Descripcion
+              </label>
+              <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="descripcion" placeholder="Ingrese la descripción"></textarea>
+            </div>
+            <div class="mb-4">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="fecha_entrega">
+                Fecha Entrega
+              </label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="fecha_entrega" type="date">
+            </div>
+            <div class="mb-4">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="fecha_recoleccion">
+                Fecha Recoleccion
+              </label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="fecha_recoleccion" type="date">
+            </div>
+            <div class="mb-4">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="cantidad_articulos">
+                Cantidad de Articulos
+              </label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="cantidad_articulos" type="number" placeholder="Ingrese la cantidad de artículos">
+            </div>
+            <div class="mb-4">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="costo">
+                Costo
+              </label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="costo" type="text" placeholder="Ingrese el costo">
+            </div>
+            <div class="flex items-center justify-between">
+              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                Agregar Pedido
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>      
 </x-layout>
