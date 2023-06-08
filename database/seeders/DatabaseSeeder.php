72f610a07567528bc93c7b6702ba06c186584480
Victor <victor.lvl47@gmail.com>
2023-06-08 01:11:00 -0600

    added a admin test user in the seed DB script

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Order;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,6 +20,13 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Create the test admin user
+        $test_admin_user = User::factory()->create([
+            'name' => 'Admin Test', 
+            'email' => 'admintest@mail.com', 
+            'password' => '123456'
+        ]);
 
         // Create some orders
         Order::factory(10)->create();
