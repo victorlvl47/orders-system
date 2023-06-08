2a05bf5a2b0b9c7b5c59c96c706072958abf2397
Victor <victor.lvl47@gmail.com>
2023-06-07 21:54:21 -0600

    added a cancel button and navbar to the add order view

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -1,4 +1,50 @@
 <x-layout>
+  <nav class="flex justify-between items-center mb-4">
+    <a href="/"
+        ><img class="w-24" src="{{asset('images/order-logo1.png')}}" alt="" class="logo"
+    /></a>
+    <ul class="flex space-x-6 mr-6 text-lg">
+        <li class="text-white">
+            <a href="/" class="hover:text-laravel"
+                ><i class="fa-solid fa-home"></i>
+                Regresar</a
+            >
+        </li>
+        {{-- @auth
+        <li>
+            <span class="font-bold uppercase">
+                Welcome {{auth()->user()->name}}
+            </span>
+        </li>
+        <li>
+            <a href="/posts/manage" class="hover:text-laravel"
+                ><i class="fa-solid fa-arrow-right-to-bracket"></i>
+                Manage Posts</a
+            >
+        </li>
+        <li>
+            <form class="inline" method="POST" action="/logout">
+                @csrf
+                <button type="submit">
+                    <i class="fa-solid fa-door-closed"></i> Logout
+                </button>
+            </form>
+        </li>
+        @else
+        <li>
+            <a href="/register" class="hover:text-laravel"
+                ><i class="fa-solid fa-gear"></i> Register</a
+            >
+        </li>
+        <li>
+            <a href="/login" class="hover:text-laravel"
+                ><i class="fa-solid fa-arrow-right-to-bracket"></i>
+                Login</a
+            >
+        </li>
+        @endauth --}}
+    </ul>
+</nav>
     <h1 class="mt-2 mb-2 text-4xl leading-none tracking-tight text-gray-900 md:text-5xl lg:text-1xl dark:text-white text-center">
         Agregar Pedido
     </h1>
@@ -94,6 +140,7 @@
               <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                 Agregar Pedido
               </button>
+              <a href="/" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Cancelar</a>
             </div>
           </form>
         </div>
