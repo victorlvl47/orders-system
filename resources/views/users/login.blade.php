557c0fabec89e8bbcefc8f688fdc8b63ba657b2a
Victor <victor.lvl47@gmail.com>
2023-06-08 00:33:28 -0600

    fixed typo in login form

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -90,7 +90,7 @@
             </div>
             <div class="flex items-center justify-between">
               <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                Registrarse
+                Iniciar Sesi&#243;n
               </button>
               <a href="/" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Cancelar</a>
             </div>
